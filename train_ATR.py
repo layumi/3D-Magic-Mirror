fa4df0ea6b5f9aa9c3930b012c0a440b7f8c343e
layumi <zdzheng12@gmail.com>
2021-11-17 04:46:31 +1100

    update -15~15

--- train_ATR.py
+++ train_ATR.py
@@ -65,7 +65,7 @@ parser.add_argument('--lambda_ic', type=float, default=0.1, help='parameter')
 parser.add_argument('--lambda_lc', type=float, default=0.001, help='parameter')
 parser.add_argument('--reg', type=float, default=0.0, help='parameter')
 parser.add_argument('--azi_scope', type=float, default=360, help='parameter')
-parser.add_argument('--elev_range', type=str, default="0~30", help='~ separated list of classes for the lsun data set')
+parser.add_argument('--elev_range', type=str, default="-15~15", help='~ separated list of classes for the lsun data set')
 parser.add_argument('--dist_range', type=str, default="2~6", help='~ separated list of classes for the lsun data set')
 
 opt = parser.parse_args()
