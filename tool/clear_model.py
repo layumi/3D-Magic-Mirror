9b138689afc11f00e305c3c7959cfe029dbcdeeb
layumi <zdzheng12@gmail.com>
2022-01-24 01:17:58 +1100

    update remove

--- tool/clear_model.py
+++ tool/clear_model.py
@@ -4,6 +4,11 @@ root = '../log/'
 
 for f in os.listdir(root):
     dir_name = root+f
+    model_path = dir_name+'/ckpts/latest_ckpt.pth'
+    #if not os.path.isfile(model_path):
+    #    print(dir_name) # remove empty folders
+    #    os.rmdir(dir_name)
+    #os.remove(dir_name+'/ckpts/latest_ckpt.pth')
     for ff in os.listdir(dir_name):
         if 'epoch' in ff:
             epoch = int(ff.split('_')[1])
