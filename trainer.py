3a4c68358ba212f728841a01141f12a3010d6237
layumi <zdzheng12@gmail.com>
2022-01-25 20:33:36 +1100

    update template

--- trainer.py
+++ trainer.py
@@ -583,7 +583,8 @@ def trainer(opt, train_dataloader, test_dataloader):
             last_delta_vertices = current_delta_vertices * 1.0 / count 
             last_delta_vertices[last_delta_vertices>0.1] = 0.1 # clip
             last_delta_vertices[last_delta_vertices<-0.1] = -0.1 # clip
-            netE.vertices_init.data += opt.em_step*last_delta_vertices
+            netE.vertices_init.data += warm_up*opt.em_step*last_delta_vertices
+            opt.em_step = opt.em_step*0.99 # decay
         netE.train()
 
     ###### After training, test the swa result
