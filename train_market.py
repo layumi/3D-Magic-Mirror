e20f77cec51451dba9ea24408a8cea0c30ae22c5
layumi <zdzheng12@gmail.com>
2022-01-31 21:47:07 +1100

    update default param

--- train_market.py
+++ train_market.py
@@ -78,7 +78,7 @@ parser.add_argument('--swa_lr', type=float, default=0.0003, help='swa learning r
 parser.add_argument('--lambda_gan', type=float, default=0.0001, help='parameter')
 parser.add_argument('--ganw', type=float, default=1, help='parameter for Xir. Since it is hard.')
 parser.add_argument('--lambda_reg', type=float, default=0.1, help='parameter')
-parser.add_argument('--lambda_edge', type=float, default=0.1, help='parameter')
+parser.add_argument('--lambda_edge', type=float, default=0.001, help='parameter')
 parser.add_argument('--lambda_deform', type=float, default=0.1, help='parameter')
 parser.add_argument('--lambda_flipz', type=float, default=0.1, help='parameter')
 parser.add_argument('--lambda_data', type=float, default=1.0, help='parameter')
