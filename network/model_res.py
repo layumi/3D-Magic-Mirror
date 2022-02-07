45a00d0b0d67d4c7272913354856081785b11bc3
layumi <zdzheng12@gmail.com>
2022-02-07 20:57:43 +1100

    update makeup

--- network/model_res.py
+++ network/model_res.py
@@ -362,23 +362,27 @@ class TextureEncoder(nn.Module):
             up6 = [nn.Dropout2d(droprate/2)] + up6 # small drop for dense prediction
 
         if self.makeup==1:
-            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation == 'none', padding_mode='zeros', coordconv = coordconv),
+            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation = 'none', padding_mode='zeros', coordconv = coordconv),
                                       ResBlock(32, norm='in'), ResBlock(32, norm='in'),
+                                      nn.Dropout2d(droprate/2),
                                       Conv2dBlock(32, 3, 3, 1, 1, norm='none', activation='none', padding_mode='reflect'),
                                       nn.Sigmoid()])
         elif self.makeup==2:
-            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation == 'none', padding_mode='zeros', coordconv = coordconv),
+            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation = 'none', padding_mode='zeros', coordconv = coordconv),
                                       ResBlock(32, norm='bn'), ResBlock(32, norm='bn'),
+                                      nn.Dropout2d(droprate/2),
                                       Conv2dBlock(32, 3, 3, 1, 1, norm='none', activation='none', padding_mode='reflect'),
                                       nn.Sigmoid()])
         elif self.makeup==3:
-            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation == 'none', padding_mode='zeros', coordconv = coordconv),
+            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation = 'none', padding_mode='zeros', coordconv = coordconv),
                                       ResBlock(32, norm='ln'), ResBlock(32, norm='ln'),
+                                      nn.Dropout2d(droprate/2),
                                       Conv2dBlock(32, 3, 3, 1, 1, norm='none', activation='none', padding_mode='reflect'),
                                       nn.Sigmoid()])
         elif self.makeup==4:
-            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation == 'none', padding_mode='zeros', coordconv = coordconv),
+            self.make = nn.Sequential(*[Conv2dBlock(3, 32, 5, 1, 2, norm='none', activation = 'none', padding_mode='zeros', coordconv = coordconv),
                                       ResBlock(32, norm='none'), ResBlock(32, norm='none'),
+                                      nn.Dropout2d(droprate/2),
                                       Conv2dBlock(32, 3, 3, 1, 1, norm='none', activation='none', padding_mode='reflect'),
                                       nn.Sigmoid()])
 
