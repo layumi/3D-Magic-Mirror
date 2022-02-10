332e6ab3bb904984afa48d4fec8c4bba3f40aea7
layumi <zdzheng12@gmail.com>
2022-02-10 18:20:55 +1100

    limit back to 0.5

--- network/model_res.py
+++ network/model_res.py
@@ -270,7 +270,7 @@ class ShapeEncoder(nn.Module):
         x = self.encoder2(x.squeeze()) # 32x3x642
         delta_vertices = x.permute(0, 2, 1).reshape(bnum, -1) # 32x (642x3)
         delta_vertices = self.linear3(delta_vertices) # all points. init is close to 0
-        delta_vertices = torch.tanh(delta_vertices) # limit the bias within [-1, 1]
+        delta_vertices = 0.5 * torch.tanh(delta_vertices) # limit the bias within [-0.5, 0.5]
         #print(delta_vertices.shape)
         return delta_vertices.view(bnum, self.num_vertices, 3)
 
