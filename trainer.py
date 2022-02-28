06eaad666988cbd691e6819ee07984be383f3087
layumi <zdzheng12@gmail.com>
2022-03-01 01:54:20 +1100

    to use same person different cloth

--- trainer.py
+++ trainer.py
@@ -203,7 +203,8 @@ def trainer(opt, train_dataloader, test_dataloader):
                         beta = min(1.0, opt.beta) # + 0.8*epoch/opt.niter)
                         alpha = torch.FloatTensor(np.random.beta(beta, beta, batch_size))
                         alpha_texture = alpha.view(batch_size, 1, 1, 1).cuda()
-                        alpha = torch.FloatTensor(np.random.beta(beta, beta, batch_size))
+                        #torch.FloatTensor(np.random.beta(beta, beta, batch_size))
+                        alpha = 1-alpha  # to use different  shape and texture pair on purporse
                         alpha_shape = alpha.view(batch_size, 1, 1 ).cuda()
                     else:
                         alpha_texture = torch.empty((batch_size, 1, 1, 1), dtype=torch.float32).uniform_(0.0, 1.0).cuda()
