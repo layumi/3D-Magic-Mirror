d7c0a554091324e3cb3729ddce4d20881ffe97fe
Jian Ma <jma3@janus0.ihpc.uts.edu.au>
2022-07-01 14:46:41 +1000

    update cub

--- datasets/bird.py
+++ datasets/bird.py
@@ -57,7 +57,7 @@ class CUBDataset(data.Dataset):
     def __getitem__(self, index):
         if len(self.selected_index) >0:
             index = self.selected_index[index]
-        index = index%self.imgs
+        index = index%len(self.imgs)
         img_path, label = self.imgs[index]
         target_height, target_width = self.image_size, self.image_size
 
