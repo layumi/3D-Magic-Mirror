321b7f4151e7051da0831d3414176982e750347f
uts9 <zdzheng@uts9>
2022-08-13 20:25:54 +1000

    update edge demo

--- prepare_cub_edge.py
+++ prepare_cub_edge.py
@@ -1,6 +1,6 @@
 import glob
 import os
-import accimage
+#import accimage
 from PIL import Image, ImageFilter, ImageOps
 import numpy as np
 # You only need to change this line to your dataset download path
@@ -18,6 +18,13 @@ for img_path in seg_list:
     edge = edge.filter(ImageFilter.SMOOTH_MORE)
     edge = edge.point(lambda p: p > 20 and 255)
     edge.save(img_path.replace('.png','_edge.png'))
+
+
+    w, h = seg.width, seg.height
+    coarse_edge = np.asarray(seg) - np.asarray(seg.resize((w//2,h//2)).resize((w, h)))
+    coarse_edge = Image.fromarray(np.uint8(np.abs(coarse_edge)))
+    coarse_edge.save(img_path.replace('.png','_coarse_edge.png'))
+
     print(img_path)
     break
 
