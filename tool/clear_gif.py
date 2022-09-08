67c2c35e412815db672e3cfea0da2e13ff839cd5
zzd@ljl <zdzheng12@gmail.com>
2022-09-08 12:48:01 +1000

    update clear tool

--- tool/clear_gif.py
+++ tool/clear_gif.py
@@ -12,7 +12,7 @@ for f in os.listdir(root):
     for ff in os.listdir(dir_name):
         if 'epoch' in ff:
             epoch = int(ff.split('_')[1])
-            if (not epoch%20 == 0) or epoch==0:
+            if (not epoch%20 == 0) or epoch<100:
                 print(dir_name+'/'+ff)
                 os.remove(dir_name+'/'+ff)
 
