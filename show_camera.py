b07cab0ae9c2b4dc71f6f540ef9cb9430078ca15
layumi <zdzheng12@gmail.com>
2022-09-30 22:16:19 +1000

    rm negative distance

--- show_camera.py
+++ show_camera.py
@@ -354,9 +354,6 @@ if __name__ == '__main__':
             vutils.save_image(textures.detach(),
                     '%s/current_textures.png' % (opt.outf), normalize=True, nrow = nrow)
 
-            #vutils.save_image(Ea.detach(),
-            #        '%s/current_edge.png' % (opt.outf), normalize=True)
-
             Ae = deep_copy(Ae, detach=True)
             vertices = Ae['vertices']
             faces = diffRender.faces
@@ -447,7 +444,7 @@ if __name__ == '__main__':
             loop.set_description('Drawing Dib_Renderer SphericalHarmonics (Gif_dist)')
             A_tmp = deep_copy(Ae, detach=True)
             for delta_dist in loop:
-                A_tmp['distances'] = - torch.tensor([delta_dist], dtype=torch.float32).repeat(opt.batchSize).cuda()
+                A_tmp['distances'] = torch.tensor([delta_dist], dtype=torch.float32).repeat(opt.batchSize).cuda()
                 predictions, _ = diffRender.render(**A_tmp)
                 predictions = predictions[:, :3]
                 image = vutils.make_grid(predictions, nrow=nrow)
@@ -464,7 +461,7 @@ if __name__ == '__main__':
             loop.set_description('Drawing Dib_Renderer SphericalHarmonics (Gif_biases)')
             A_tmp = deep_copy(Ae, detach=True)
             for delta_biases in loop:
-                A_tmp['azimuths'] = torch.tensor([0], dtype=torch.float32).repeat(opt.batchSize).cuda()
+                #A_tmp['azimuths'] = torch.tensor([0], dtype=torch.float32).repeat(opt.batchSize).cuda()
                 A_tmp['biases'] = torch.tensor([delta_biases, 0], dtype=torch.float32).unsqueeze(0).repeat(opt.batchSize,1).cuda()
                 predictions, _ = diffRender.render(**A_tmp)
                 predictions = predictions[:, :3]
@@ -473,7 +470,7 @@ if __name__ == '__main__':
                 image = (image * 255.0).astype(np.uint8)
                 writer.append_data(image)
             for delta_biases in loop:
-                A_tmp['azimuths'] = torch.tensor([0], dtype=torch.float32).repeat(opt.batchSize).cuda()
+                #A_tmp['azimuths'] = torch.tensor([0], dtype=torch.float32).repeat(opt.batchSize).cuda()
                 A_tmp['biases'] = torch.tensor([0, delta_biases], dtype=torch.float32).unsqueeze(0).repeat(opt.batchSize,1).cuda()
                 predictions, _ = diffRender.render(**A_tmp)
                 predictions = predictions[:, :3]
