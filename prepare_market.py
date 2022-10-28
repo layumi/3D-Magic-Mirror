a5dbf8682fa59dfcb3a8616e695c7e330f0ceb56
Jian Ma <jma3@venus23.ihpc.uts.edu.au>
2022-10-28 21:42:17 +1100

    update market

--- prepare_market.py
+++ prepare_market.py
@@ -6,6 +6,7 @@ import numpy as np
 download_path = '../Market/hq/seg_hmr' 
 
 seg_list = glob.glob(os.path.join(download_path,'*/*/*.png'))
+percentage_list = []
 
 for img_path in seg_list:
     seg = Image.open(img_path).convert('L')
@@ -14,11 +15,12 @@ for img_path in seg_list:
     percentage = foreground/(seg.size[0]*seg.size[1]) 
     new_name = img_path.replace('.png','_%.2f.png'%percentage)
     print(img_path, new_name)
-    os.system('mv %s %s'%( img_path, new_name) )
-
+    #os.system('mv %s %s'%( img_path, new_name) )
+    percentage_list.append(percentage)
     #edge = seg.filter(ImageFilter.FIND_EDGES)
     #edge = edge.filter(ImageFilter.SMOOTH_MORE)
     #edge = edge.point(lambda p: p > 20 and 255)
     #edge.save(img_path.replace('.png','_edge.png'))
     #break
 
+print(sum(percentage_list)/len(percentage_list))
